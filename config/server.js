35269c5a74dcf9f2218c55a6ebd8d66106d3c273
Luis Andino <luisandino2002@gmail.com>
2020-12-21 23:51:12 -0600

    second commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '02c829db27e113010f9e0260af84e217'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2a5862d9-5d9d-4cd5-ba68-870460a4779e'
+};
